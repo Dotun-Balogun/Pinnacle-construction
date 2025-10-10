d08e340d34e81d8de38b5c608d3e7420edc35714
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-10-10 21:22:10 +0100

    removed unused variable

--- components/sections/Hero2.tsx
+++ components/sections/Hero2.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import slide2 from '../../assets/images/slide-2.jpg'
+// import slide2 from '../../assets/images/slide-2.jpg'
 const Hero2 = () => {
   return (
     <div 
