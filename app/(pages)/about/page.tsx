9607469f50b82690c27f5fa3ba60d7441cca74b3
Dotun-Balogun <balogunlightonolamidotun@gmail.com>
2025-11-03 23:12:20 +0100

    about section countup

--- app/(pages)/about/page.tsx
+++ app/(pages)/about/page.tsx
@@ -7,6 +7,7 @@ import ServiceCard_2 from '@/components/ui/card/ServiceCard_2'
 import { servicesData } from '@/app/data/ServiceData_2'
 import Image from 'next/image'
 import background from '@/assets/images/row-bg.jpg'
+import safetyImage from '@/assets/images/portfolio6-1.jpg'
 import StatCard from '@/components/ui/card/StatCard'
 
 const features = [
@@ -17,6 +18,14 @@ const features = [
   { id: 5, text: 'Offset against future profits.' },
   { id: 6, text: 'Profits can be reinvested & paid' },
 ]
+
+
+const stats = [
+    { id: 1, label: "Projects Completed", end: 120 , suffix:'+'},
+    { id: 2, label: "Happy Clients", end: 85 },
+    { id: 3, label: "Experts Memebers", end: 12, suffix:"K" },
+    { id: 4, label: "Awards Won", end: 15 },
+  ];
 
 const AboutSection = () => {
   return (
@@ -111,14 +120,76 @@ const AboutSection = () => {
 
 
 
-      <div className="grid grid-cols-2lg:grid-cols-4 place-items-cente">
-        <StatCard/>
+      <div className="  grid grid-cols-2  lg:grid-cols-4 place-items-center gap-10">
+        {stats.map((item) => (
+          <StatCard key={item.id} end={item.end} title={item.label} suffix={item.suffix} />
+        ))}
+      </div>
+        
+        
       </div>
     
 
 
+        </div>
+        <div className=" flex flex-col justify-between  md:flex-row-reverse   lg:min-h-screen">
+      
+      <div className='flex flex-col w-full p-4 lg:gap-y-4 '>
+        <FaintText backgroundText={'Safety'} mainText='CONSTRUCTION SAFETY FIRST'/>
+        <h3 className="text-[28px] font-bold text-center lg:text-[50px] md:text-left">Alway dedicated and devoted</h3>
+        <div className="p-4">
+          <p className="text-[20px] text-[#8b8b8b] text-center md:text-left">
+            Slogan creation for your construction company is crucial for the
+            overall branding. And branding in today’s world can’t be ignored, We
+            hope that the above Construction Slogans. Don’t forget to share it
+            on social media and related building communities.
+          </p>
+        </div>
+       <div className='flex flex-col gap-4'>
+        <div className='lg:w-[600px] bg-gray-300 flex items-center h-[46px]'>
+          <div className='relative h-[46px] lg:w-[500px] bg-secondary text-[20px] font-bold flex items-center text-white p-4'>
+
+          Safety construction
+          <span className='absolute left-full w-[61px] h-[35px] bg-primary text-secondary font-bold -translate-x-1/2 flex items-center justify-center'> 90%</span>
           </div>
         </div>
+
+        <div className='lg:w-[600px] bg-gray-300 flex items-center h-[46px]'>
+          <div className='relative h-[46px] lg:w-[500px] bg-secondary text-[20px] font-bold flex items-center text-white p-4'>
+
+          Maintenance
+          <span className='absolute left-full lg:lg:w-[61px] h-[35px] bg-primary text-secondary font-bold -translate-x-1/2 flex items-center justify-center'> 90%</span>
+          </div>
+        </div>
+
+        <div className='lg:w-[600px] bg-gray-300 flex items-center h-[46px]'>
+          <div className='relative h-[46px] lg:w-[400px] bg-secondary text-[20px] font-bold flex items-center text-white p-4'>
+
+          Road construction
+          <span className='absolute left-full w-[61px] h-[35px] bg-primary text-secondary font-bold -translate-x-1/2 flex items-center justify-center'> 80%</span>
+          </div>
+        </div>
+      
+       </div>
+      </div>
+      <div className="relative bg-primary lg:bg-white w-full h-[380px] md:h-[500px] lg:max-w-[700px] lg:h-[741px] p-5 lg:p-0  ">
+        <div className="hidden lg:block relative lg:bg-primary w-full h-[380px] lg:max-w-[500px] lg:h-[741px]">
+
+        </div>
+        <motion.div
+        whileHover={{
+          rotateY:20
+        }}
+         className="lg:h-[600px]  h-[350px] md:h-[450px] lg:left-0 lg:top-1/2 lg:-translate-y-1/2 w-full relative lg:absolute">
+       <Image
+       src={safetyImage}
+      fill
+      sizes='h-full w-full'
+       alt="about"
+       />
+        </motion.div>
+      </div>
+    </div>
       </div>
     </section>
   )
--- components/ui/card/StatCard.tsx
+++ components/ui/card/StatCard.tsx
@@ -1,14 +1,36 @@
+'use client'
 import React from 'react'
+import CountUp from "react-countup";
+import { useInView } from "react-intersection-observer";
 
-const StatCard = () => {
+type StatCardProps = {
+  end: number;
+  title: string;
+  suffix?: string;
+};
+
+const StatCard = ({ end, title, suffix }: StatCardProps) => {
+  const { ref, inView } = useInView({
+    triggerOnce: true,
+    threshold: 0.4,
+  });
+
   return (
-    <div  className='relative bg-white lg:w-[315px] lg:h-[228px] flex items-center justify-center '>
-        <h1 className='text-[80px]'></h1>
-        <div className="bg-primary p-2 font-medium absolute text-seocondary top-1/2 -translate-y-1/2 flex items-center justify-center">
-   <h1 className='text-[20px]'>Years of Experience</h1>
-        </div>
+    <div
+      ref={ref}  
+      className='relative group hover:bg-primary bg-white w-full w-[150px] h-[150px] lg:w-[315px] lg:h-[228px]  flex items-center justify-center px-4'
+    >
+      <h1 className='relative text-secondary text-4xl lg:text-[80px]'>
+        {inView ? <CountUp end={end} duration={3} /> : 0} {suffix}
+      </h1>
+<div className='absolute text-secondary p-2  w-full top-full -translate-y-1/2 flex items-center justify-center'>
+ <div className=" w-full h-[50px] bg-primary group-hover:bg-white font-medium text-secondary  flex items-center justify-center">
+        <h1 className=' text-base lg:text-[20px] text-center'>{title}</h1>
+      </div>
+</div>
+     
     </div>
-  )
-}
+  );
+};
 
-export default StatCard
+export default StatCard;
